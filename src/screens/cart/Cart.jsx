3cf413d0a82207ea4ca99cc3473f45591046c681
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-28 01:29:55 +0530

    Created chat component with static data

--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -1,8 +1,31 @@
+import { useSelector } from "react-redux";
 import "./Cart.css";
 const Cart = () => {
+  const { cartItems } = useSelector((state) => state.cartReducer);
+  console.log(cartItems);
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="cart">
+      <div className="cart__shoppingCart">
+        <h4>Shopping Cart</h4>
+        <div className="cart__items">
+          <div className="cart__item__info">
+            <p>name</p>
+            <p>price:</p>
+            <p>quantity</p>
+          </div>
+          <div className="cart__items__right">
+            <img
+              src="https://avatars.githubusercontent.com/u/45279411?v=4"
+              alt="image"
+            />
+            <p>X</p>
+          </div>
+        </div>
+      </div>
+      <div className="cart__subtotal">
+        <h4>Subtotal: ₹ 333</h4>
+        <button>Pay Now</button>
+      </div>
     </div>
   );
 };
