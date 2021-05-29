b9c2800645f5223ad8aa83a40bd9df6cf41522ab
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-29 21:54:31 +0530

    Added dropdown for toggling between user name and logout

--- src/components/navabar/Navbar.jsx
+++ src/components/navabar/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
 import "./Navbar.css";
 const Navbar = () => {
   const { cartItems } = useSelector((state) => state.cartReducer);
-  console.log(cartItems);
+  const { currentUser } = useSelector((state) => state.loginUserReducer);
   return (
     <div className="navbar">
       <div className="logo">
@@ -12,10 +13,33 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="right">
-        <Link className="link" to="/login" style={{ textDecoration: "none" }}>
-          <h5>Login</h5>
-        </Link>
+        {!currentUser ? (
+          <Link className="link" to="/login" style={{ textDecoration: "none" }}>
+            <h5>Login</h5>
+          </Link>
+        ) : (
+          <div className="dropdown">
+            <a
+              className="dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              {currentUser.name}
+            </a>
 
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#">
+                Orders
+              </a>
+              <a className="dropdown-item" href="#">
+                Logout
+              </a>
+            </div>
+          </div>
+        )}
         <Link className="link" to="/cart" style={{ textDecoration: "none" }}>
           <h6 className="cart">Cart {cartItems.length}</h6>
         </Link>
